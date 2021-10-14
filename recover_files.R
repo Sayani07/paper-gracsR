d49fec51a4d09f3d39e3056e99ddc23c4b5bfc4f
Sayani Gupta <Sayani.Gupta@monash.edu>
2021-10-14 16:55:35 +1100

    files without rds

---
+++ recover_files.R
@@ -0,0 +1,9 @@
+write_rds(wkndwday, "data/dist_gran_wkndwday_356cust_robust.rds")
+write_rds(moy, "data/dist_gran_moy_356cust.rds")
+write_rds(hod, "data/dist_gran_hod_356cust_robust.rds")
+write_rds(elec_356_raw, "data/elec_nogap_2013_clean_356cust.rds")
+write_rds(elec_600_wpd, "data/algo2-cust600-wpd-rawdata.rds")
+
+
+
+
