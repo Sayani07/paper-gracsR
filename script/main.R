4453aa6274aa8a760bd771e840c346b3a545e424
Sayani Gupta <Sayani.Gupta@monash.edu>
2021-11-07 13:39:10 +1100

    mds plot square aspect ratio and caption change

--- script/main.R
+++ script/main.R
@@ -1068,7 +1068,8 @@ mds_plot_10 <- ggplot(all_data,
   theme_bw()+
   theme(legend.position = "bottom") +
   scale_color_brewer(palette = "Dark2")+
-  ylab("mds1") + xlab("mds2")
+  ylab("mds1") + xlab("mds2")+
+  coord_fixed()
 
 mds_plot_10
 
